630223e2afbd14161c51c9ed2fb92365f471af34
Kyle (Ky) James <kyle._.james@outlook.com>
2023-09-29 11:17:13 -0500

    finished about and resume page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-
+ 
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
@@ -16,8 +16,8 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+// technologies variables
+const technologiesItem = document.querySelectorAll("[data-technologies-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -28,30 +28,30 @@ const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
-const testimonialsModalFunc = function () {
+const technologiesModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
 // add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
+for (let i = 0; i < technologiesItem.length; i++) {
 
-  testimonialsItem[i].addEventListener("click", function () {
+  technologiesItem[i].addEventListener("click", function () {
 
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalImg.src = this.querySelector("[data-technologies-avatar]").src;
+    modalImg.alt = this.querySelector("[data-technologies-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-technologies-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-technologies-text]").innerHTML;
 
-    testimonialsModalFunc();
+    technologiesModalFunc();
 
   });
 
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+modalCloseBtn.addEventListener("click", technologiesModalFunc);
+overlay.addEventListener("click", technologiesModalFunc);
 
 
 
